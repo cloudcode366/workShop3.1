caeaf3fd603fa1b9fb5f09d4d086678efa146635
cloudcode366 <tgnhatminh.forwork@gmail.com>
2022-09-25 00:19:00 +0700

    code cu quen save

--- src/main/java/com/pack1/Tester.java
+++ src/main/java/com/pack1/Tester.java
@@ -13,7 +13,7 @@ public class Tester {
         Car c2 = new Car("red", 100, true, true);
         c2.pressAcceleratorButton();
         c2.setColour("black");
-        System.out.println("Colour of c3:" + c2.getColour());
+        System.out.println("Colour of c2:" + c2.getColour());
         c2.output();
     }
 }
